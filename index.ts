c122fdacfdebfff05678683a175620b583a4d57a
Emanuel Phillipe Ribeiro <153885104+emanuel-phillipe@users.noreply.github.com>
2024-02-19 01:40:04 +0000

    Added front-end URL

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const app: Express = express();
 
 app.use((req:Request, res:Response, next:NextFunction) => {
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'https://seraphix-grades-front.vercel.app')
   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization')
 
