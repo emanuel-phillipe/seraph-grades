b69b70719146f8a00c64c22f96bf97e3dc271b64
Emanuel Phillipe Ribeiro <153885104+emanuel-phillipe@users.noreply.github.com>
2024-02-19 18:31:24 +0000

    Added CORS settings

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ app.use((req:Request, res:Response, next:NextFunction) => {
   res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization')
 
-  app.use(cors())
+  app.use(cors({credentials: true}))
 
   next()
 })
